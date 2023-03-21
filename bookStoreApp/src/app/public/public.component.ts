e70709356ca9fe6c3bc33dc80ef5b66516fdb9eb
mithun <mithunj43@gmail.com>
2023-03-21 19:30:05 +0530

    Get Param Value in parent & child

--- bookStoreApp/src/app/public/public.component.ts
+++ bookStoreApp/src/app/public/public.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
   styleUrls: ['./public.component.scss']
 })
-export class PublicComponent {
+export class PublicComponent implements OnInit{
 
   bookId:number =21;
   authorId:number=25;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.firstChild?.params.subscribe(param => {
+      console.log(param)
+    });
+  }
 }
