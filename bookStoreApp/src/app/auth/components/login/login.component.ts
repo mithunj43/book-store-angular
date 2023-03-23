039a357a9d26922a27c3859a02879f856360561b
mithun <mithunj43@gmail.com>
2023-03-23 12:24:14 +0530

    Navigate to a URL from component file

--- bookStoreApp/src/app/auth/components/login/login.component.ts
+++ bookStoreApp/src/app/auth/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { query } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   public name: string = '';
   public lastName: string = '';
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(queryparam => {
@@ -19,7 +19,14 @@ export class LoginComponent implements OnInit {
       this.lastName = queryparam['lastname'];
       console.log(queryparam);
     });
-
   }
 
+  goToSignup():void{
+    this.router.navigate(['/auth/signup']);
+  }
+
+  goToBookDetails(id:number, authorId:number):void{
+    this.router.navigate(['/public/book-details',id,'author',authorId],
+    {queryParams:{name:'Pooja Mithun', email:'poo'}});
+  }
 }
