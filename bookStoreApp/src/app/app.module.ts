ecfa4fd31d8a12e7d9ca82e69adc233748bef524
mithun <mithunj43@gmail.com>
2023-03-23 21:35:53 +0530

    Lazy loading

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { PublicModule } from './public/public.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -18,9 +15,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AuthModule,
-    UserModule,
-    PublicModule,
+    // AuthModule,
+    // UserModule,
+    // PublicModule,
     SharedModule,
     AppRoutingModule
   ],
