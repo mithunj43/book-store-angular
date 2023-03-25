27558af205cff5a66a14b35ec23a532715d5b1e7
mithun <mithunj43@gmail.com>
2023-03-25 19:34:29 +0530

    Create separate component for angular material

--- bookStoreApp/src/app/shared/shared.module.ts
+++ bookStoreApp/src/app/shared/shared.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RouterModule } from '@angular/router';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports: [
+    ToolbarComponent
   ]
 })
 export class SharedModule { }
