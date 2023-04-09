289efccc48ef7a40f8c792410d70c605589f5e0f
mithun <mithunj43@gmail.com>
2023-04-09 12:39:20 +0530

    ngAfterContentInit vs ngAfterContentChecked

--- bookStoreApp/src/app/shared/components/authors/authors.component.ts
+++ bookStoreApp/src/app/shared/components/authors/authors.component.ts
@@ -1,28 +1,36 @@
-import { Component, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, ContentChild, DoCheck, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { __values } from 'tslib';
 import { AuthorModel } from '../../models/authors.model';
 import { TestService } from '../../services/test.service';
+import { AuthorsAddressComponent } from '../authors-address/authors-address.component';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.scss']
 })
-export class AuthorsComponent implements OnInit, OnChanges, DoCheck {
+export class AuthorsComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked {
 
   @Input() data: number;
   @Input() data2: boolean;
   @Input() author: AuthorModel;
+  @ContentChild(AuthorsAddressComponent) authAddress:AuthorsAddressComponent;
 
   constructor() {
     console.log('Hello from Child constructor');
   }
+  ngAfterContentInit(): void {
+    console.log('After content Init', this.authAddress?.address)
+  }
+  ngAfterContentChecked(): void {
+    console.log('After content checked', this.authAddress?.address)
+  }
   ngDoCheck(): void {
-    console.log('Docheck', this.author)
+    //console.log('Docheck', this.author)
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes)
+    //console.log(changes)
   }
 
   ngOnInit(): void {
